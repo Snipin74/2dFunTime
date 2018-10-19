03799d7a3d7899db2d906a414e425aebc00eb565
Snipin74 <josh.cove74@gmail.com>
2018-10-19 14:50:46 -0400

    shark move
    sharks

---
+++ 2d fun time/Assets/Scripts/SharkGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkGenerator : MonoBehaviour {
+
+    public GameObject theShark;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float sharkWidth;
+
+	// Use this for initialization
+	void Start () {
+        sharkWidth = theShark.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + sharkWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            Instantiate(theShark, transform.position, transform.rotation);
+
+            
+        }
+	}
+}
