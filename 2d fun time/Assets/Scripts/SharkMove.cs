24640a90f2d00550919958af86f7f08bccf4a939
Snipin74 <josh.cove74@gmail.com>
2018-10-19 13:46:33 -0400

    shark
    have one shark will have a pool of them hopefully by te end of the day

---
+++ 2d fun time/Assets/Scripts/SharkMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkMove : MonoBehaviour {
+
+    public int moveShark = 10;
+    [SerializeField] private float speed;
+
+    public Transform target;
+    public Transform target1;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+    }
+}
