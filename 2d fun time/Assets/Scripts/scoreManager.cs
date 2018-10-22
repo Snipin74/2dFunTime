7e2d5cc3a9dbea04369f3277f93ee3d0c1cb437d
Snipin74 <josh.cove74@gmail.com>
2018-10-22 16:38:59 -0400

    ui is almost done
    score is counting im about to have the collectable done also

---
+++ 2d fun time/Assets/Scripts/scoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreManager : MonoBehaviour {
+
+    public Text scoreText;
+    public Text hiScoreText;
+
+    public float scoreCount;
+    public float hiScoreCount;
+
+    public float pointsPerSecond;
+    public bool scoreIncreasing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        scoreCount += pointsPerSecond * Time.deltaTime;
+
+        scoreText.text = "Score: " + Mathf.Round (scoreCount);
+        hiScoreText.text = "High Score: " + Mathf.Round (hiScoreCount); 
+	}
+}
